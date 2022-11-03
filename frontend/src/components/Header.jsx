bb8d79d3f47a75912137ee0ff1e3b950f7a542d6
alnil <alniltanudtanud@gmail.com>
2022-11-04 05:49:49 +0800

    Registration Form Created

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+
+
+function Header() {
+  return (
+    <header className='header'>
+        <div className='logo'>
+        <Link to='/'>Mary-Galas.com</Link>
+        </div>
+        <ul>
+            <li>
+                <Link to='/login'>
+                    <FaSignInAlt /> Login 
+                </Link>
+            </li>
+            <li>
+                <Link to='/register'>
+                    <FaUser /> Register
+                </Link>
+            </li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+import {FaUser} from 'react-icons/fa'
+
+// react use states
+function Register() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    })
+
+    const {name, email, password, password2} = formData
+    const onChange = () => {}
+
+
+  return <>
+
+  <section className="heading">
+    <h1>
+                <FaUser /> Register   
+    </h1>
+
+    <p>Create an account here</p>
+  </section>
+  <section className='form'>
+        <form>
+            
+           <div className='form-group'>
+           <input 
+            type="text" 
+            className="form-control" 
+            id="name"
+            name="name"
+            value={name}
+            placeholder="Enter your name"
+            onChange={onChange}/>
+           </div>
+
+           <div className='form-group'>
+           <input 
+            type="email" 
+            className="form-control" 
+            id="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={onChange}/>
+           </div>
+
+           <div className='form-group'>
+           <input 
+            type="password" 
+            className="form-control" 
+            id="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={onChange}/>
+           </div>
+
+           <div className='form-group'>
+           <input 
+            type="password" 
+            className="form-control" 
+            id="password2"
+            name="password2"
+            value={password2}
+            placeholder="confirm password"
+            onChange={onChange}/>
+           </div>
+
+           <div className="form-group">
+            <button type="submit" className='btn btn-block'>Submit</button>
+           </div>
+
+        </form>
+  </section>
+  </>
+  
+}
+
+export default Register
